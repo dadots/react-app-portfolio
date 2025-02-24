ce5b0fbc9501014aec6c9f0546bff6d8796f6d57
Dadots <bacatedariuspaul@gmail.com>
2025-02-24 18:29:56 +0800

    Updates

--- assets/convertTimelinePositionToClass-431e1589.js
+++
@@ -1,1 +0,0 @@
-import{r as e,e as r}from"./index-9f57e151.js";const t=e.createContext({});function s(e){return"alternate-reverse"===e?"positionAlternateReverse":`position${r(e)}`}export{t as T,s as c};
---
+++ assets/index-a4a93826.js
@@ -0,0 +1,19 @@
+import{s as e,R as i,_ as n,j as r,W as t,T as o,D as s}from"./index-bce7f662.js";import{e as _}from"./constants-656c87d1.js";const d=e.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    align-items: center;
+    padding-bottom: 50px;
+    background-color: ${({theme:e})=>e.card_light};
+`,c=e.div`
+    width: 100%;
+    max-width: 1000px;
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+`,a=i.lazy((()=>n((()=>import("./ExperienceCards-9f852593.js")),["assets/ExperienceCards-9f852593.js","assets/index-bce7f662.js","assets/index-29a84fde.css"]))),p=i.lazy((()=>n((()=>import("./index-8d2b6712.js")),["assets/index-8d2b6712.js","assets/index-bce7f662.js","assets/index-29a84fde.css","assets/convertTimelinePositionToClass-238638c4.js"]))),x=i.lazy((()=>n((()=>import("./index-98195f7a.js")),["assets/index-98195f7a.js","assets/index-bce7f662.js","assets/index-29a84fde.css","assets/convertTimelinePositionToClass-238638c4.js","assets/timelineContentClasses-4b795a3a.js","assets/constants-656c87d1.js"]))),l=i.lazy((()=>n((()=>import("./index-7eedc50a.js")),["assets/index-7eedc50a.js","assets/index-bce7f662.js","assets/index-29a84fde.css"]))),m=i.lazy((()=>n((()=>import("./index-190637f2.js")),["assets/index-190637f2.js","assets/index-bce7f662.js","assets/index-29a84fde.css"]))),E=i.lazy((()=>n((()=>import("./index-7b6a64a6.js")),["assets/index-7b6a64a6.js","assets/index-bce7f662.js","assets/index-29a84fde.css","assets/convertTimelinePositionToClass-238638c4.js","assets/timelineContentClasses-4b795a3a.js"]))),h=i.lazy((()=>n((()=>import("./index-fa698daa.js")),["assets/index-fa698daa.js","assets/index-bce7f662.js","assets/index-29a84fde.css"]))),j=()=>r.jsx(d,{id:"experience",children:r.jsxs(t,{children:[r.jsx(o,{children:"Experience"}),r.jsx(s,{children:"Below are some of my experiences base on my resume."}),r.jsx(c,{children:r.jsx(p,{children:_.map(((e,i)=>r.jsxs(x,{children:[r.jsxs(l,{children:[r.jsx(h,{variant:"outlined",sx:{borderColor:"#00DFA2"}}),i!==_.length-1&&r.jsx(m,{sx:{background:"-webkit-linear-gradient(225deg, rgb(0, 223, 162) 0%, rgb(1, 106, 112) 100%)"}})]}),r.jsx(E,{sx:{py:"12px",px:2},children:r.jsx(a,{experience:e})})]},i)))})})]})});export{j as default};
